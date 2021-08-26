cca59d861af7ddf16cb3b9b642d3cfb2399c4e89
Zhivko Zhelev <zhivko.zhelev@egt-digital.com>
2021-08-26 10:42:04 +0300

    initial commit

---
+++ src/types.d.ts
@@ -0,0 +1,10 @@
+declare module 'styled-dropdown-component';
+declare module 'styled-react-modal';
+declare module 'react-datepicker';
+declare module '*.svg'
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
+declare module '*.gif'
+declare module '*.bmp'
+declare module '*.tiff'
